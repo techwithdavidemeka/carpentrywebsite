ebde5ea02957d6eda200ba7044905e9b96f109d7
darkanon <mobylestreem200@gmail.com>
2023-12-23 12:17:10 +0100

    made some updates in different pages, and added scroll animation

--- script.js
+++ script.js
@@ -143,7 +143,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const activePage = window.location.pathname
 const navLinks = document.querySelectorAll('nav a').forEach(link =>{
-    if (link.href.includes (activePage)) {
+        if (link.href.includes (activePage)) {
         link.classList.add('active');
     }
 })
